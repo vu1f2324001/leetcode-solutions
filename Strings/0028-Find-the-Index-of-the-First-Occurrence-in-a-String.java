523274ac4b21fd65fba32e8b73a8f9367c777c17
Akshada Dadaso Valkunde <202756237+vu1f2324001@users.noreply.github.com>
2026-07-14 20:01:08 +0530

    Implement strStr method to find first occurrence

--- Strings/0028-Find-the-Index-of-the-First-Occurrence-in-a-String.java
+++ Strings/0028-Find-the-Index-of-the-First-Occurrence-in-a-String.java
@@ -1,1 +1,14 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
 
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+
+            if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                return i;
+            }
+
+        }
+
+        return -1;
+    }
+}
