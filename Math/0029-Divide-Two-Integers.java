5a1f11d2bd40d7b7221149b25fe7d5de9dc0f533
Akshada Dadaso Valkunde <202756237+vu1f2324001@users.noreply.github.com>
2026-07-17 20:09:28 +0530

    Implement divide function for two integers

--- Math/0029-Divide-Two-Integers.java
+++ Math/0029-Divide-Two-Integers.java
@@ -1,1 +1,35 @@
 
+class Solution {
+    public int divide(int dividend, int divisor) {
+
+        // Overflow case
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        // Sign of answer
+        boolean negative = (dividend < 0) ^ (divisor < 0);
+
+        // Convert to positive long
+        long dvd = Math.abs((long) dividend);
+        long dvs = Math.abs((long) divisor);
+
+        int result = 0;
+
+        while (dvd >= dvs) {
+
+            long temp = dvs;
+            int multiple = 1;
+
+            while (dvd >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+
+            dvd -= temp;
+            result += multiple;
+        }
+
+        return negative ? -result : result;
+    }
+}
