4548bdb8fb2a66d5ba6e9f7d9769c62cbb83222c
Akshada Dadaso Valkunde <202756237+vu1f2324001@users.noreply.github.com>
2026-07-30 15:43:51 +0530

    Implement Count and Say algorithm in Java

---
+++ Strings/0038-Count-and-Say.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String countAndSay(int n) {
+
+        String ans = "1";
+
+        for (int i = 2; i <= n; i++) {
+
+            StringBuilder temp = new StringBuilder();
+
+            int count = 1;
+
+            for (int j = 1; j < ans.length(); j++) {
+
+                if (ans.charAt(j) == ans.charAt(j - 1)) {
+                    count++;
+                } else {
+                    temp.append(count);
+                    temp.append(ans.charAt(j - 1));
+                    count = 1;
+                }
+            }
+
+            temp.append(count);
+            temp.append(ans.charAt(ans.length() - 1));
+
+            ans = temp.toString();
+        }
+
+        return ans;
+    }
+}
