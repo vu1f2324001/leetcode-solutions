4f9fccdc5e291808af96e9762257bd59920085c6
Akshada Dadaso Valkunde <202756237+vu1f2324001@users.noreply.github.com>
2026-08-01 18:11:15 +0530

    Add combinationSum2 method for unique combinations
    Implement backtracking algorithm to find unique combinations of candidates that sum to the target.

---
+++ Backtracking/0040-Combination-Sum-II.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+
+public List<List<Integer>> combinationSum2(int[] candidates, int target) {  
+
+    List<List<Integer>> ans = new ArrayList<>();  
+
+    Arrays.sort(candidates);  
+
+    backtrack(candidates, target, 0, new ArrayList<>(), ans);  
+
+    return ans;  
+}  
+
+private void backtrack(int[] candidates, int target, int start,  
+                       List<Integer> current, List<List<Integer>> ans) {  
+
+    if (target == 0) {  
+        ans.add(new ArrayList<>(current));  
+        return;  
+    }  
+
+    if (target < 0) {  
+        return;  
+    }  
+
+    for (int i = start; i < candidates.length; i++) {  
+
+  
+        if (i > start && candidates[i] == candidates[i - 1]) {  
+            continue;  
+        }  
+
+        if (candidates[i] > target) {  
+            break;  
+        }  
+
+        current.add(candidates[i]);  
+
+      
+        backtrack(candidates, target - candidates[i], i + 1, current, ans);  
+
+        current.remove(current.size() - 1);  
+    }  
+}
+
+}
