89b2d66f1866cd91a74fe028f553a8606f225699
Akshada Dadaso Valkunde <202756237+vu1f2324001@users.noreply.github.com>
2026-08-13 20:50:41 +0530

    Implement myPow function to calculate x^n

---
+++ Math/0050-Pow-x-n.java
@@ -0,0 +1,26 @@
+class Solution {
+public double myPow(double x, int n) {
+
+long power = n;  
+
+    if (power < 0) {  
+        x = 1 / x;  
+        power = -power;  
+    }  
+
+    double ans = 1;  
+
+    while (power > 0) {  
+
+        if (power % 2 == 1) {  
+            ans = ans * x;  
+        }  
+
+        x = x * x;  
+        power = power / 2;  
+    }  
+
+    return ans;  
+}
+
+}
