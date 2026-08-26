06fcb9ce6f67862af24f00206dc56fe770d74f0b
Akshada Dadaso Valkunde <202756237+vu1f2324001@users.noreply.github.com>
2026-08-26 17:41:26 +0530

    Implement getPermutation method for sequence generation

---
+++ Math/0060-Permutation-Sequence.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+public String getPermutation(int n, int k) {
+
+List<Integer> numbers = new ArrayList<>();  
+
+      
+    for (int i = 1; i <= n; i++) {  
+        numbers.add(i);  
+    }  
+
+      
+    int fact = 1;  
+
+    for (int i = 1; i < n; i++) {  
+        fact = fact * i;  
+    }  
+
+      
+    k = k - 1;  
+
+    StringBuilder ans = new StringBuilder();  
+
+    while (n > 0) {  
+
+          
+        int index = k / fact;  
+
+        ans.append(numbers.get(index));  
+        numbers.remove(index);  
+
+          
+        k = k % fact;  
+
+        n--;  
+
+          
+        if (n > 0) {  
+            fact = fact / n;  
+        }  
+    }  
+
+    return ans.toString();  
+}
+
+}
