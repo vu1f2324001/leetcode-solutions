3de7dbe59075bd5bf80b8a63d345713bb7f462e9
Akshada Dadaso Valkunde <202756237+vu1f2324001@users.noreply.github.com>
2026-08-31 14:38:57 +0530

    Implement valid number checker in Java

---
+++ Strings/0065-Valid-Number.java
@@ -0,0 +1,47 @@
+class Solution {
+public boolean isNumber(String s) {
+boolean digit = false;
+boolean dot = false;
+boolean exp = false;
+boolean expDigit = true;
+
+for (int i = 0; i < s.length(); i++) {  
+        char c = s.charAt(i);  
+
+        if (Character.isDigit(c)) {  
+            digit = true;  
+
+            if (exp) {  
+                expDigit = true;  
+            }  
+        }   
+        else if (c == '+' || c == '-') {  
+            if (i > 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') {  
+                return false;  
+            }  
+            if (i == s.length() - 1) {  
+                return false;  
+            }  
+        }   
+        else if (c == '.') {  
+            if (dot || exp) {  
+                return false;  
+            }  
+            dot = true;  
+        }   
+        else if (c == 'e' || c == 'E') {  
+            if (exp || !digit) {  
+                return false;  
+            }  
+            exp = true;  
+            expDigit = false;  
+        }   
+        else {  
+            return false;  
+        }  
+    }  
+
+    return digit && expDigit;  
+}
+
+}
