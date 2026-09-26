2e7efbf5e67f91c2a0ea10f3d6977653ad6e950e
Akshada Dadaso Valkunde <202756237+vu1f2324001@users.noreply.github.com>
2026-09-26 14:20:02 +0530

    Add maximalRectangle method for binary matrix
    Implement a solution to find the maximal rectangle in a binary matrix using a stack-based approach.

---
+++ Stack/0085-Maximal-Rectangle.java
@@ -0,0 +1,53 @@
+class Solution {
+public int maximalRectangle(char[][] matrix) {
+int rows = matrix.length;
+int cols = matrix[0].length;
+
+int[] heights = new int[cols];  
+    int maxArea = 0;  
+
+    for (int i = 0; i < rows; i++) {  
+
+        for (int j = 0; j < cols; j++) {  
+            if (matrix[i][j] == '1') {  
+                heights[j]++;  
+            } else {  
+                heights[j] = 0;  
+            }  
+        }  
+
+        maxArea = Math.max(maxArea, largestRectangleArea(heights));  
+    }  
+
+    return maxArea;  
+}  
+
+private int largestRectangleArea(int[] heights) {  
+    Stack<Integer> stack = new Stack<>();  
+    int maxArea = 0;  
+
+    for (int i = 0; i <= heights.length; i++) {  
+        int currentHeight = (i == heights.length) ? 0 : heights[i];  
+
+        while (!stack.isEmpty() && currentHeight < heights[stack.peek()]) {  
+            int height = heights[stack.pop()];  
+
+            int width;  
+
+            if (stack.isEmpty()) {  
+                width = i;  
+            } else {  
+                width = i - stack.peek() - 1;  
+            }  
+
+            int area = height * width;  
+            maxArea = Math.max(maxArea, area);  
+        }  
+
+        stack.push(i);  
+    }  
+
+    return maxArea;  
+}
+
+}
